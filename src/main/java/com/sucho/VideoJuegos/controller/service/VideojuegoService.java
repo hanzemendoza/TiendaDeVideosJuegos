c62dfcf1d2cd8486ae3997836d192387fec1874b
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2020-06-10 15:46:13 -0300

    se agrego el package service y dentro de la misma la clase VideojuegoService

---
+++ src/main/java/com/sucho/VideoJuegos/controller/service/VideojuegoService.java
@@ -0,0 +1,41 @@
+
+package service;
+
+import com.sucho.VideoJuegos.controller.domain.Videojuego;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideojuegoService {
+    public List<Videojuego> buscarDestacado(){
+        List<Videojuego> destacados = new ArrayList<>();
+        
+        Videojuego juego = new Videojuego();
+        juego.setNombre("Super Mario Bros");
+        juego.setDescripcion("Primer juego ya siendo el fontanero plataformero");
+        juego.setImagenUrl("img/mario1.jpg");
+        destacados.add(juego);
+        
+        juego = new Videojuego();
+        juego.setNombre("Super Mario Bros 2");
+        juego.setDescripcion("Primer juego ya siendo el fontanero plataformero");
+        juego.setImagenUrl("img/mario2.png");
+        destacados.add(juego);
+        
+        juego = new Videojuego();
+        juego.setNombre("Super Mario Bros 3");
+        juego.setDescripcion("Primer juego ya siendo el fontanero plataformero");
+        juego.setImagenUrl("img/mario3.jpg");
+        destacados.add(juego);
+        
+        juego = new Videojuego();
+        juego.setNombre("Super Mario World");
+        juego.setDescripcion("Primer juego ya siendo el fontanero plataformero");
+        juego.setImagenUrl("img/marioworld.jpg");
+        destacados.add(juego);
+        
+        
+        return destacados;
+    }
+}
